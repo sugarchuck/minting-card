fa435c794b987ae4acd3e659dbc7a3f0497f4324
mauridevops <contacto@mauriciooyola.com>
2022-01-27 19:01:12 -0300

    card

--- src/components/Minter.js
+++ src/components/Minter.js
@@ -15,7 +15,7 @@ const initialInfoState = {
 
 const initialMintState = {
   loading: false,
-  status: `Mint your ${contract.name} to get 20% value back on everyone's Mint and win 100 BNB! Accumulate Mints for more Chances to win `,
+  status: `Mint your ${contract.name} to get 20% value back on everyone's Mint and win 100 BNB! Accumulate Mints for more Chances to win. (Verifiable Random Function powered by Chainlink)`,
   amount: 1,
   supply: "0",
   cost: "0",
