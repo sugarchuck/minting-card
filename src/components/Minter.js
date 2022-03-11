fe05ba367ccc240a52f2f3c1ba2660379451a756
mauridevops <contacto@mauriciooyola.com>
2022-03-11 15:29:02 -0300

    dskfjndskj

--- src/components/Minter.js
+++ src/components/Minter.js
@@ -15,7 +15,7 @@ const initialInfoState = {
 
 const initialMintState = {
   loading: false,
-  status: `Mint your Awesome ${contract.name}! `,
+  status: ` First Presale cost: 0.001 || Second Presale cost: 0.002 || Public Sale cost: 0.003 || Mint your Awesome ${contract.name}! `,
   amount: 1,
   supply: "0",
   cost: "0",
@@ -237,9 +237,6 @@ function Minter() {
                   {info.web3?.utils.fromWei(mintInfo.cost, "ether") *
                     mintInfo.amount}{" "}
                   {contract.chain_symbol}
-                  First Presale cost: 0.001
-                  Second Presale cost: 0.002
-                  Public Sale cost: 0.003
                 </p>
                 <div style={{ width: 20 }}></div>
                 <p style={{ color: "var(--statusText)", textAlign: "center" }}>
